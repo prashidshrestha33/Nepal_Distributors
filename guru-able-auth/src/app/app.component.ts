061130a3ea69317f2e6aa6cd3c060f5dad4795f6
Prashid Shrestha <prashidshrestha33@gmail.com>
2025-12-04 19:10:37 +0545

    updating angular vs from repository

--- guru-able-auth/src/app/app.component.ts
+++ guru-able-auth/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Component({
-  selector: 'app-root',
-  template: `<router-outlet></router-outlet>`
+    selector: 'app-root',
+    template: `<router-outlet></router-outlet>`,
+    standalone: false
 })
 export class AppComponent {
   constructor(public auth: AuthService) {}
