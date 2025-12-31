edb2173aed03d58558b612a0a4458b1103bf1858
Nabin Kandel <nabin.kandel@oic.edu.np>
2025-12-31 12:04:10 +0545

    Update catalog type

---
+++ marketplace-nepal-backend/src/Marketplace.Model/Models/CatalogTypeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Model.Models
+{
+    public class CatalogTypeDto
+    {
+        public string CatalogType { get; set; }
+        public  int DisplayOrder { get; set; }
+    }
+}
